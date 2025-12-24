d66b96672f8bb64fd9ec2f2e548496a774d0ccce
pjblecha <44296698+pjblecha@users.noreply.github.com>
2025-12-24 17:32:47 -0600

    completed lesson 37

--- src/test/java/com/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
+++ src/test/java/com/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
@@ -3,19 +3,27 @@ package com.guru.springframework.spring6di.controllers;
 import com.guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class SetterInjectedControllerTest {
 
+    @Autowired
     SetterInjectedController setterInjectedController;
 
+    /*
+    Once I have @Autowired the controller and loaded the Spring context with the class annotation,
+    This becomes superfluous
     @BeforeEach
     void setUp() {
         setterInjectedController = new SetterInjectedController();
         // If this is null, you will get a NPE... comment it out to try
         setterInjectedController.setGreetingService(new GreetingServiceImpl());
     }
+    */
 
     @Test
     void sayHello() {
