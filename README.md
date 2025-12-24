65024959e19e4eeebc65981fd70cd71059219b15
pjblecha <44296698+pjblecha@users.noreply.github.com>
2025-12-24 15:25:52 -0600

    initial project build

---
+++ README.md
@@ -0,0 +1,3 @@
+# SPRING6DI
+
+Project to learn Dependency Injection in Spring Framework 6(7)
---
+++ src/test/java/com/guru/springframework/spring6di/Spring6diApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.springframework.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6diApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
